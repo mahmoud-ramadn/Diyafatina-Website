5bbf1c9263cddc191c8c01ae42ce84a398ee5906
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-06-14 01:09:15 +0300

    swiper screen-shot

--- components/SwiperConent.vue
+++ components/SwiperConent.vue
@@ -1,104 +1,91 @@
 <template>
-  <div class="relative py-16">
-    <div class="container flex items-center justify-center">
-      <Swiper
-        :modules="[Autoplay]"
-        :space-between="20"
-        :speed="600"
-        :centered-slides="true"
-        :loop="true"
-        :autoplay="{ delay: 2500, disableOnInteraction: false }"
-        :breakpoints="{
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            centeredSlides: true,
-          },
-          480: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-            centeredSlides: true,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 0,
-            centeredSlides: true,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 0,
-            centeredSlides: true,
-          },
-          1440: {
-            slidesPerView: 5,
-            spaceBetween: 0,
-            centeredSlides: true,
-          },
-        }"
-        class="max-w-[900px] mx-auto relative h-[400px]"
-      >
-        <SwiperSlide v-for="(image, index) in images" :key="index">
-          <img
-            :src="image"
-            :alt="`Slide ${index + 1}`"
-            class="rounded-lg transition-all duration-300 object-cover"
-          />
-        </SwiperSlide>
+  <div class="swiper-container">
+    <Swiper v-bind="swiperOptions" class="swiper">
+      <SwiperSlide v-for="(image, index) in images" :key="index">
+        <img :src="image" :alt="`Slide ${index + 1}`" class="slide-image" />
+      </SwiperSlide>
 
-        <img
-          src="/assets/images/screen shot/Mask group.jpg"
-          alt="Mobile Frame"
-          class="absolute top-[48%] left-1/2 -translate-y-1/2 -translate-x-1/2 "
-        />
-      </Swiper>
-    </div>
+      <img
+        src="/assets/images/screen shot/Mask group.jpg"
+        alt=""
+        class="mobile-frame"
+        aria-hidden="true"
+      />
+    </Swiper>
   </div>
 </template>
 
 <script setup lang="ts">
+// Swiper imports
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/vue";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
 
-// Import images
+// Image imports
 // eslint-disable-next-line import/no-duplicates
 import img1 from "../assets/images/swiper/three.png";
 // eslint-disable-next-line import/no-duplicates
 import img6 from "../assets/images/swiper/three.png";
 import img2 from "../assets/images/swiper/الرئيسية – 2.png";
-// eslint-disable-next-line import/no-duplicates
 import img3 from "../assets/images/swiper/دورات حضورية (3).png";
 // eslint-disable-next-line import/no-duplicates
-import img5 from "../assets/images/swiper/دورات حضورية (3).png";
+import img5 from "../assets/images/swiper/دورات حضورية.png";
+// eslint-disable-next-line import/no-duplicates
 import img4 from "../assets/images/swiper/دورات حضورية.png";
 
-// Sample images array with imported images
-const images = [img1, img2, img3, img4, img5, img6];
+const images = [img1, img2, img4, img3, img4, img6];
+
+// Swiper configuration
+const swiperOptions = {
+  modules: [Autoplay],
+  spaceBetween: 20,
+  speed: 600,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    320: { slidesPerView: 1, spaceBetween: 10 },
+    480: { slidesPerView: 1, spaceBetween: 0 },
+    768: { slidesPerView: 3, spaceBetween: 0 },
+    1024: { slidesPerView: 3, spaceBetween: 0 },
+    1250: { slidesPerView: 3, spaceBetween: 0 },
+    1440: { slidesPerView: 5, spaceBetween: 0 },
+  },
+};
 </script>
 
-<style>
-.swiper-slide-active img {
-  @apply lg:w-[194px] lg:h-[90%] w-[191px] object-cover;
+<style scoped>
+.swiper-container {
+  @apply relative py-16;
+}
+
+.swiper {
+  @apply max-w-[900px] mx-auto relative h-[375px] flex items-center justify-center;
+}
+
+.slide-image {
+  @apply rounded-lg transition-all duration-300 object-cover;
+}
+
+.mobile-frame {
+  @apply absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 h-[86%] overflow-hidden;
 }
 
 .swiper-slide {
-  @apply flex items-center justify-center;
+  @apply flex items-center justify-center w-fit;
 }
 
 .swiper-slide:not(.swiper-slide-active) img {
-  @apply w-[134px];
+  @apply scale-90;
 }
 
 .swiper-slide-active {
-  @apply flex items-center justify-center;
-
-  @apply opacity-100 scale-100 z-50;
-}
-.swiper {
-  @apply flex items-center justify-center;
+  @apply flex items-center justify-center opacity-100 scale-100 z-50;
 }
 </style>
